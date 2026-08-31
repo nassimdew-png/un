e914801e112a12a9a37bddf74e29d65d6ee066be
nassimdew-png <nassimdew@gmail.com>
2026-08-31 04:24:45 +0200

    fix: make PersonalAccessToken extend SanctumPersonalAccessToken

--- backend/app/Models/PersonalAccessToken.php
+++ backend/app/Models/PersonalAccessToken.php
@@ -2,47 +2,9 @@
 
 namespace App\Models;
 
-use MongoDB\Laravel\Eloquent\Model;
-use Laravel\Sanctum\Contracts\HasAbilities;
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-class PersonalAccessToken extends Model implements HasAbilities
+class PersonalAccessToken extends SanctumPersonalAccessToken
 {
-    protected $connection = 'mongodb';
-    protected $collection = 'personal_access_tokens';
-
-    protected $fillable = [
-        'name',
-        'token',
-        'abilities',
-        'expires_at',
-        'tokenable_id',
-        'tokenable_type',
-        'last_used_at',
-    ];
-
-    protected $casts = [
-        'abilities' => 'json',
-        'last_used_at' => 'datetime',
-        'expires_at' => 'datetime',
-    ];
-
-    protected $hidden = [
-        'token',
-    ];
-
-    public function tokenable()
-    {
-        return $this->morphTo();
-    }
-
-    public function can($ability)
-    {
-        $abilities = $this->abilities ?? [];
-        return in_array('*', $abilities) || in_array($ability, $abilities);
-    }
-
-    public function cant($ability)
-    {
-        return !$this->can($ability);
-    }
+    protected $table = 'personal_access_tokens';
 }
